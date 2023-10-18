cf232f40a6566e8749fce87d024719a5d24aa355
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-18 17:13:52 +0200

    added style to form and response access

--- intro.php
+++ intro.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Revise steps Php</title>
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="./styles/style.css">
+    <!-- FONTAWESOME LINK VERSION 6.4.2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
     <form action="./my_server.php" method='GET'>
-        <input type="text" name="name" id="name"> <br>
-        <input type="email" name="email" id="email"> <br>
-        <input type="number" name="age" id="age"> <br>
+        <h1>
+            Insert your data to Sign In
+        </h1>
+        <input type="text" name="name" id="name" placeholder="Insert you're name"> <br>
+        <input type="email" name="email" id="email" placeholder="Insert you're email"> <br>
+        <input type="number" name="age" id="age" placeholder="Insert you're email"> <br>
         <input type="submit" value="send">
     </form>
 </body>
--- my_server.php
+++ my_server.php
@@ -1,5 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+<style>
+    .denied-response{
+    color: red;
+    text-align:center;
+    margin-top:5rem;
+}
+.fa-xmark{
+    color:red;
+    margin-bottom:5rem;
+}
+.fa-check{
+    margin-bottom:5rem;
+}
+.correct-response{
+    color:lightgreen;
+    text-align:center;
+    margin-top:5rem;
+}
+</style>
+    
+</body>
+</html>
 
 <?php
+
+
 
 $name = $_GET['name'];
 $email = $_GET['email'];
@@ -8,12 +41,14 @@ $age = $_GET['age'];
 if(empty($name) || empty($email) || empty($age)){
     echo 'Please insert a value';
 }
-elseif(strlen($name) < 3 || !is_numeric($age) || ($age < 18) || !str_contains($email,'@')){
-    echo 'Access Denied';
+elseif(strlen($name) < 3 || !is_numeric($age) || ($age < 18) || ($age > 90) || !str_contains($email,'@')){
+    echo "<h1 class='denied-response'><i class='fa-solid fa-xmark fa-2x'></i> <br>
+    Access Denied Please insert Correct Values <h1>";
 }
 else{
-    echo 'Access Granted';
+    echo "<h1 class='correct-response'><i class='fa-solid fa-check fa-2x'></i> <br> Access Granted Your'e signed <br> Welcome {$name}</h1>";
 }
+
 
 
 
